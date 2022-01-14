3d8df6880f40863e7066c01aa039f27320a073b3
stacycharat <stacycharat@stacys-macbook.attlocal.net>
2022-01-13 18:29:07 -0800

    pizza pizza, the riddler

--- src/_03_if_else/_5_pizza/pizza/pizza.pde
+++ src/_03_if_else/_5_pizza/pizza/pizza.pde
@@ -1,4 +1,4 @@
-
+PImage pepperoni;
 void setup() {
     size(500,500);
     fill(239,204,162);
@@ -7,9 +7,20 @@ void setup() {
     ellipse(250,250,370,370);
     fill(255, 234, 0);
     ellipse(250,250,340,340);
-    PImage pepperoni = loadImage("pepperoni.png");
-    pepperoni.resize(30,30);
+    pepperoni = loadImage("pepperoni.png");
+    pepperoni.resize(50,50);
 }
 void draw() {
-    image(pepperoni ,100,200);
+  if (mousePressed) {
+    image(pepperoni,100,200);
+    image(pepperoni,190,280);
+    image(pepperoni,290,300);
+    image(pepperoni,250,130);
+    image(pepperoni,140,140);
+    image(pepperoni,250,350);
+    image(pepperoni,350,200);
+    image(pepperoni,200,200);
+    image(pepperoni,270,220);
+    image(pepperoni,120,300);
+  }
 }
--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -1,4 +1,7 @@
 package _04_int._1_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
@@ -9,8 +12,37 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+int score = 0;
+int fail = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
+String riddle = JOptionPane.showInputDialog(null, "riddle me this:"
+		+ "Brothers and sisters I have none but this man's father is my father's son.\n"
+		+ "Who is the man?");
+if (riddle.equals("my son")) {
+JOptionPane.showMessageDialog(null, "Correct!");
+score++;
+JOptionPane.showMessageDialog(null, "Your score is... "+score+" right and "+fail+" wrong!");
+}else {
+	JOptionPane.showMessageDialog(null, "Wrong! "
+			+ "Correct Answer:  																							"
+			+ "my son");
+	fail++;
+	JOptionPane.showMessageDialog(null, "Your score is... "+score+" right and "+fail+" wrong!");
+}
+String riddle2 = JOptionPane.showInputDialog(null, "riddle me this:																"
+		+ "What can travel around the world while staying in a corner?");
+if(riddle2.equals("a stamp")) {
+	JOptionPane.showMessageDialog(null, "Correct!");
+	score++;
+	JOptionPane.showMessageDialog(null, "Your score is... "+score+" right and "+fail+" wrong!");
+	}else {	
+		JOptionPane.showMessageDialog(null, "Wrong! "
+				+ "Correct Answer:  																							"
+				+ "a stamp");
+		fail++;
+		JOptionPane.showMessageDialog(null, "Your score is... "+score+" right and "+fail+" wrong!");
+		}
+String riddle3 = JOptionPane.showInputDialog(null, "riddle me this: ");
 
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
 
@@ -20,6 +52,7 @@ public class TheRiddler {
 
 		// 2. Make a pop up to show the score.
 		
+JOptionPane.showMessageDialog(null, "Your final score is... "+score+" right and "+fail+" wrong!");
 	}
 }
 
